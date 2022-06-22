72d94adb48710898a5d8628c721e4ae49f2bffa9
k26k <klee26a@gmail.com>
2022-06-22 18:20:37 +0900

    push

--- js/common.js
+++ js/common.js
@@ -1,7 +1,35 @@
-$(function(){
-    $(".product").mouseover(function(){
-        $(this).children("button").stop().css("opacity","1")
-    }).mouseout(function(){
-        $(this).children("button").stop().css("opacity","0")
-    })
-})
+$(document).ready(function(){
+
+
+    // ======== header 시작 ========
+    let mainHeader = ".main_wrap .main_nav1";
+    $(window).scroll(function(){
+        let winS = $(window).scrollTop()*1;
+        console.log(winS);
+        if(winS <= 100){
+            $(mainHeader).removeClass("header_fixed");
+            $(mainHeader).css({
+                "height":(200-winS)+"px"
+            });
+        }else{
+            $(mainHeader).addClass("header_fixed");
+        }
+    });
+
+    $(".main_nav1_login").children().click(function(){
+        console.log("click login");
+        $("body").append("<div class='demo'></div>");
+        $(".demo").css({
+            "width":"100%", "height":"100%", "background":"rgba(255,0,0,0.2)",
+            "position":"fixed", "top":"0", "left":"0", "z-index":"5000"
+        });
+        $(".demo").append("<div></div>")
+        $(".demo div").css({
+            "width":"80%", "height":"auto", "background":"rgba(0,0,255,0.4)", "padding":"10px",
+            "position":"fixed", "top":"10%", "left":"10%", "z-index":"6000"
+        });
+        $(".demo>div").load("./login_test.html");
+        $(".demo").click(function(){$(".demo").remove()});
+    });
+    // ======== header 끝   ========
+});

e292e966ff3e70e28585b3cb803446d3bc8be6af
k26k <klee26a@gmail.com>
2022-06-27 03:54:55 +0900

    up

--- js/common.js
+++ js/common.js
@@ -1,6 +1,15 @@
-$(document).ready(function(){
+$(function(){
+    console.log("log");
 
-    // ======== header 시작 ========
+    // 메인페이지 찜 버튼
+    $(".product").mouseover(function(){
+        $(this).children("button").stop().css("opacity","1")
+    }).mouseout(function(){
+        $(this).children("button").stop().css("opacity","0")
+    })
+    // 메인페이지 찜 버튼 끝
+
+ // ======== header 시작 ========
     // 접근할 요소 변수로 선언
     let mainHeader = "header";
     // 스크롤시 header height 조정, 아이콘 변경 프로그램
@@ -8,7 +17,7 @@ $(document).ready(function(){
         let winS = $(window).scrollTop();
         let screenW = $(window).width();
         // console.log(winS);
-        console.log(screenW);
+        // console.log(screenW);
         if(winS < 30){
             $(mainHeader).height((150-winS)+"px");
         }else if(winS < 80){
@@ -23,7 +32,6 @@ $(document).ready(function(){
     // ======== login popup 시작 ========
     // 로그인 클릭시 body에 빈 div를 생성하여 추가함
     // 이후 div에 새로운 div를 추가하고 요소 안에 login_test.html를 추가함
-    
     $(".fa-user").click(function(){
         console.log("click login");
         $("body").append("<div class='demo'></div>");
@@ -34,12 +42,19 @@ $(document).ready(function(){
         $(".demo").append("<div></div>")
         $(".demo div").css({
             "width":"90%", "height":"90%", "border":"10px solid #ccc", 
-            "border-radius":"20px", 
-            "position":"fixed", "top":"5%", "left":"5%", "overflow":"auto"
+            "border-radius":"20px", "background-color":"#fff",
+            "position":"relative", "top":"5%", "left":"5%", "overflow":"auto"
         });
-        $(".demo>div").load("./login_test.html");
-        $(".demo>div").focus().click(function(){$(".demo").remove()});
+        $(".demo>div").load("./login.html");
+        $(".demo").append("<div class='btnX'>X</div>")
+        $(".demo .btnX").css({
+            "width":"50px", "height":"50px", "border":"5px solid #ccc", 
+            "border-radius":"10px", "background-color":"#aaa",
+            "text-align":"center","line-height":"40px", "font-size":"40px", "color":"#fff",
+            "position":"absolute", "top":"5%", "right":"5%", "overflow":"auto", "z-index":"7000"
+        });
+        $(".demo>div").focus();
+        $(".demo .btnX").click(function(){$(".demo").remove()});
     });
     // ======== login popup 끝   ========
-    
-});
+})
--- js/shipping_check.js
+++ js/shipping_check.js
@@ -1,13 +1,17 @@
+console.log("shipping_check start");
+
 $.ajax({
-    type: "get", //데이터를 읽어오는 방식
+    // type: "get", //데이터를 읽어오는 방식
     url: "./json/shipping.json", //요청할 url, 또는 파일명
     dataType: "json" , //＂서버에서 전송받을 데이터형식",
     success: function(data){// 정상 요청, 응답 시 처리
         console.log("succese");
         console.log(data);
-        $(".shipping_list").append("<p>"+data[1][0].key()+"</p>");
+        // $(".shipping_list").append("<p>"+data[1][0].key()+"</p>");
     },
     error: function(xhr){
-        console.log("error");
-    },
+        console.error(xhr.status +"/"+ xhr.errorText);
+    }
 });
+
+console.log("shipping_check end");
